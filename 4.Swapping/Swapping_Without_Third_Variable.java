22983d228680aa9a6808b1e1ab355f5b8550a7fc
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-07-25 16:23:05 +0530

    Adding Swapping Program

---
+++ 4.Swapping/Swapping_Without_Third_Variable.java
@@ -0,0 +1,15 @@
+public class Swapping_Without_Third_Variable {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        a = a + b; // 30
+        b = a - b; // 10
+        a = a - b; // 20
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
